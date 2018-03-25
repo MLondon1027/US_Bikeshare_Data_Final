0b3816877c61dabf1267bd306cb65c6e58b33d77
MLondon1027 <36179660+MLondon1027@users.noreply.github.com>
2018-03-24 21:35:08 -0400

    Update bikeshareproject.py

--- bikeshareproject.py
+++ bikeshareproject.py
@@ -201,9 +201,9 @@ def main():
         user_stats(df)
 
        	restart = input('\nWould you like to restart? Enter yes or no.\n')
-    	while restart not in ('yes', 'no'):
+        while restart not in ('yes', 'no'):
             restart = input('\nWould you like to restart? Enter yes or no.\n')
-    	if restart.lower() != 'yes':
+        if restart.lower() != 'yes':
             break
 
 
